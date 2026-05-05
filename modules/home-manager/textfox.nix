1bbbcf2e38ac697498c1a6a9c90e357f891a49f5
Randi Budi <hi@randibudi.dev>
2026-05-05 21:32:42 +0700

    fix(textfox): set firefox configPath to .mozilla/firefox

--- modules/home-manager/textfox.nix
+++ modules/home-manager/textfox.nix
@@ -1,6 +1,8 @@
 {inputs, ...}: {
   imports = [inputs.textfox.homeManagerModules.default];
 
+  programs.firefox.configPath = ".mozilla/firefox";
+
   textfox = {
     enable = true;
     profiles = ["default"];
