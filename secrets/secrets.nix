3b0282309f1ecd4a8db3e4b875183fc9902eb4c5
Randi Budi Prananda <contact@randibudi.dev>
2026-08-11 00:39:37 +0700

    chore(secrets): update SSH public key and re-encrypt age secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,5 +1,5 @@
 let
-  randibudi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIK14V26VEvwRFT6fU5VAS45kRRABjj5EBdSuVCRc8C9 hi@randibudi.dev";
+  randibudi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIII4yhniahwSjB1OWJbK3rZdU040yhmGEAleCOnRJ2VR";
 in {
   "vpn-l2tp-solusi247.age".publicKeys = [randibudi];
   "vpn-wireguard-wisecon.age".publicKeys = [randibudi];
